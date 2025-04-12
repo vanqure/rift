6bb756f1ca95b93f2f36b18cef403ffdabe38e3f
esoterikv <s.mecina@zsp9.elodz.edu.pl>
2025-04-12 17:41:46 +0200

    Add CachedMap and RiftMap interfaces for caching functionality

---
+++ rift-common/src/dev/esoterik/rift/map/CachedMapUpdate.java
@@ -0,0 +1,10 @@
+package dev.esoterik.rift.map;
+
+import dev.esoterik.rift.codec.Packet;
+
+public interface CachedMapUpdate extends Packet {
+
+  String getKey();
+
+  String getValue();
+}
---
+++ rift-common/src/dev/esoterik/rift/map/RiftMap.java
@@ -0,0 +1,22 @@
+package dev.esoterik.rift.map;
+
+import java.io.Serializable;
+import java.util.Map;
+import java.util.stream.Stream;
+
+public interface RiftMap<S extends Serializable, F, V extends S> {
+
+  boolean set(F field, V value);
+
+  V get(F field);
+
+  boolean del(F field);
+
+  Stream<F> fields();
+
+  Stream<V> values();
+
+  Map<F, V> entries();
+
+  long size();
+}
